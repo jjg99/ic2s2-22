9ae7a5011efd07bfc78d8925abb9a0443d6f1b35
jjg99 <javierj3@illinois.edu>
2022-07-19 15:51:43 -0500

    Initial Commit

---
+++ dict_load_graph.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''
+Carga de los datos .csv de Safegraph para ver la movilidad entre areas
+
+'''
+
+
+import json
+import os
+import pathlib
+
+from matplotlib import pyplot as plt
+
+
+currentpath = pathlib.Path().resolve()
+dict_path = str(currentpath) + "/data/dict/"
+
+complete_list = []
+
+# Loading the dictionaries already built
+for filename in os.scandir(dict_path):
+    if filename.is_file():
+        with open(filename) as f:
+            line = f.readline()
+            json_dict = json.loads(line)
+
+        print(json_dict)
+        print(type(json_dict))
+        json_list = json_dict.items()
+        json_list = sorted(json_list)
+        x, y = zip(*json_list)
+        plt.plot(x,y, label=filename)
+        
+plt.legend()
+plt.show()
